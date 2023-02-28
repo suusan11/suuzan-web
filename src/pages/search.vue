8acd0bbde3ee374b2385452951c6a7b0499d0b91
mie <gawow11@gmail.com>
2023-02-28 10:57:32 +0900

    :bug: fix posts statement

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -40,7 +40,7 @@
 
 <template>
     <div class="l-container l-inner__flex">
-        <main v-if="posts">
+        <main>
             <p class="result">「{{ query }}」の検索結果 {{ totalCount }}件</p>
             <div v-if="posts && posts.contents">
                 <PostList :posts="posts.contents" />
