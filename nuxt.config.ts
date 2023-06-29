f72bd6fee216fff8f755eda6a9eba2683f1fac92
mie <gawow11@gmail.com>
2023-06-29 14:14:28 +0900

    :green_heart: test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,10 @@ const { API_KEY, SERVICE_DOMAIN } = process.env;
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     srcDir: 'src/',
+    ssr: true,
     runtimeConfig: {
         apiKey: API_KEY,
-        serviceDomain: SERVICE_DOMAIN,
+        serviceDomain: SERVICE_DOMAIN
         // target: 'server'
     },
 
